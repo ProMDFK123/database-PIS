6471ab26aa1dd2c7b86fac14fa87d74ddb9e971d
Rodrigo <impgrodrigo@gmail.com>
2025-10-13 20:26:59 -0300

    feat(api): Creacion de controller, dto y services para obtener los detalles de la oferta

---
+++ bolsafeucn_back/src/Application/Services/Interfaces/IOfferService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bolsafeucn_back.src.Application.Services.Interfaces
+{
+    public class IOfferService
+    {
+        
+    }
+}
