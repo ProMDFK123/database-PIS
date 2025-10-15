33fbfed9b18fab332554c12de1d8dacc313079e3
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-10-15 00:25:37 -0300

    docs (dto) : add comments

---
+++ bolsafeucn_back/src/Application/DTO/DTO.cs
@@ -0,0 +1,8 @@
+namespace bolsafeucn_back.src.Application.DTOs
+{
+    public class UsuarioDto
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+    }
+}
