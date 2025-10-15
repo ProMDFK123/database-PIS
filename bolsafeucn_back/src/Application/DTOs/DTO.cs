14cb5006cbf54381d1b40d605eeeded4f99526be
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-10-15 00:26:04 -0300

    a

--- bolsafeucn_back/src/Application/DTOs/DTO.cs
+++
@@ -1,8 +0,0 @@
-namespace bolsafeucn_back.src.Application.DTOs
-{
-    public class UsuarioDto
-    {
-        public string Nombre { get; set; } = string.Empty;
-        public string Correo { get; set; } = string.Empty;
-    }
-}
