406b9ef37757e3d54992c826eea97bd8adea1eb3
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-10-15 00:25:43 -0300

    asdasd

--- bolsafeucn_back/src/Application/DTO/DTO.cs
+++ bolsafeucn_back/src/Application/DTO/DTO.cs
@@ -2,6 +2,7 @@ namespace bolsafeucn_back.src.Application.DTOs
 {
     public class UsuarioDto
     {
+        // User data transfer object properties
         public string Nombre { get; set; } = string.Empty;
         public string Correo { get; set; } = string.Empty;
     }
