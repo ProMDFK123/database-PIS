f0d96ba6b2975deb78e85c8b3a91b3d1603a8174
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-10-15 00:29:28 -0300

    feat (publication service) : add implement publication service file

---
+++ bolsafeucn_back/src/Application/Services/Implements/PublicationService.cs
@@ -0,0 +1,6 @@
+using bolsafeucn_back.src.Application.Services.Interfaces;
+
+namespace bolsafeucn_back.src.Application.Services.Implements
+{
+    public class PublicationService : IPublicationService { }
+}
